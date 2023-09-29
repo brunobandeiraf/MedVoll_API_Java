64054d6afa04c6efe56dce535123848c1de08053
Bruno Bandeira <brunobandeirafernandes@gmail.com>
2023-09-28 21:34:27 -0300

    method list

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -2,14 +2,17 @@ package med.voll.api.controller;
 
 import jakarta.validation.Valid;
 import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("medicos")
@@ -22,5 +25,16 @@ public class MedicoController {
     public void cadastrar(@RequestBody @Valid DadosCadastroMedico dados) {
         repository.save(new Medico(dados));
         // @Valid exige os métodos de validação do Bean Validation
+    }
+
+    @GetMapping
+    public Page<DadosListagemMedico> lista(@PageableDefault(size = 10, sort = {"nome"})Pageable paginacao)  {
+        return repository.findAll(paginacao).map(DadosListagemMedico::new);
+
+        // Convertendo o tipo de lista para o tipo DadosListagemmedico
+        // Paginação
+        // http://localhost:8080/medicos?size=1&page=1
+        // Ordenação
+        // http://localhost:8080/medicos?sort=nome,desc
     }
 }
